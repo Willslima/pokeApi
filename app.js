556e3979b2bf779ae89698dbc79d3df149ac8891
Willslimaa <williansantos38@gmail.com>
2020-06-03 00:23:27 -0300

    alterando a imagem do pokemon

--- app.js
+++ app.js
@@ -10,36 +10,28 @@ pokemon = () => {
     h2.innerHTML = guardaDados
     axios.get(`https://pokeapi.co/api/v2/pokemon/${guardaDados}`)
         .then(sucesso => {
-            const {name, abilities } = sucesso.data
-            console.log(abilities)
-
+            const {name, abilities, sprites } = sucesso.data
+            
             //Passando as habilidades
             for(var i = 0; i < abilities.length; i++ ){
-            // console.log(abilities[i].ability.name)
-            let habilidades = abilities[i].ability.name
-            console.log(habilidades)
-            let hab = document.getElementById('hab'+[i])
-            hab.innerHTML = habilidades
-        }
-
-        
+                // console.log(abilities[i].ability.name)
+                let habilidades = abilities[i].ability.name
+                // console.log(habilidades)
+                let hab = document.getElementById('hab'+[i])
+                hab.innerHTML = habilidades
+            }
             
-})//Fim do then
+            console.log(sprites.front_default)//pegando caminho da imagem
+            
+            //alterando a imagem do pokemon
+            let imagemDoPokemon = sprites.front_default
+            let caminhoDaImagem = document.getElementById('imagem')
+            console.log(caminhoDaImagem)
+            caminhoDaImagem.setAttribute('src', imagemDoPokemon)
+            
+})
+//Fim do then
         .catch(erro => console.log(erro))
 }
 
 
-	// 	//Criar o botão de exclusão
-	// 	let btn = document.createElement('button')
-	// 	btn.className = 'btn btn-danger'
-	// 	btn.innerHTML = '<i class="fa fa-times"  ></i>'
-	// 	btn.id = `id_despesa_${d.id}`
-	// 	btn.onclick = function(){
-	// 		let id = this.id.replace('id_despesa_','')
-	// 		//alert(id)
-	// 		bd.remover(id)
-	// 		window.location.reload()
-	// 	}
-	// 	linha.insertCell(4).append(btn)
-	// 	console.log(d)
-	// })
